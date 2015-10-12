9885d304a170e23547bc6e8ee835a6ce6dfa0444
Sverrir Pall Sverrisson <sverrirps@gmail.com>
2015-10-12 09:16:29 +0000

    Shader class implemented.

--- core/src/com/ru/tgra/shapes/BoxGraphic.java
+++ core/src/com/ru/tgra/shapes/BoxGraphic.java
@@ -1,6 +1,7 @@
  package com.ru.tgra.shapes;
 
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,6 +11,7 @@ public class BoxGraphic {
 
 	private static FloatBuffer vertexBuffer;
 	private static FloatBuffer normalBuffer;
+	private static ShortBuffer indexBuffer;
 	private static int vertexPointer;
 	private static int normalPointer;
 
@@ -73,8 +75,22 @@ public class BoxGraphic {
 							1.0f, 0.0f, 0.0f};
 
 		normalBuffer = BufferUtils.newFloatBuffer(72);
-		normalBuffer.put(normalArray);
+		BufferUtils.copy(normalArray, 0, normalBuffer, 72);
+		//normalBuffer.put(normalArray);
 		normalBuffer.rewind();
+		
+		short[] indexArray = {0, 1, 2, 0, 2, 3,
+							4, 5, 6, 4, 6, 7,
+							8, 9, 10, 8, 10, 11,
+							12, 13, 14, 12, 14, 15,
+							16, 17, 18, 16, 18, 19,
+							20, 21, 22, 20, 22, 23};
+		
+		indexBuffer = BufferUtils.newShortBuffer(36);
+		BufferUtils.copy(indexArray,  0,  indexBuffer, 36);
+		//indexBuffer.put(indexArray);
+		indexBuffer.rewind();
+		
 	}
 
 	public static void drawSolidCube() {
@@ -82,13 +98,15 @@ public class BoxGraphic {
 		Gdx.gl.glVertexAttribPointer(vertexPointer, 3, GL20.GL_FLOAT, false, 0, vertexBuffer);
 		Gdx.gl.glVertexAttribPointer(normalPointer, 3, GL20.GL_FLOAT, false, 0, normalBuffer);
 
+		Gdx.gl.glDrawElements(GL20.GL_TRIANGLES, 36, GL20.GL_UNSIGNED_SHORT, indexBuffer);
+/*
 		Gdx.gl.glDrawArrays(GL20.GL_TRIANGLE_FAN, 0, 4);
 		Gdx.gl.glDrawArrays(GL20.GL_TRIANGLE_FAN, 4, 4);
 		Gdx.gl.glDrawArrays(GL20.GL_TRIANGLE_FAN, 8, 4);
 		Gdx.gl.glDrawArrays(GL20.GL_TRIANGLE_FAN, 12, 4);
 		Gdx.gl.glDrawArrays(GL20.GL_TRIANGLE_FAN, 16, 4);
 		Gdx.gl.glDrawArrays(GL20.GL_TRIANGLE_FAN, 20, 4);
-
+*/
 	}
 
 	public static void drawOutlineCube() {
